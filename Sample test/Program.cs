5c191cfecaaae3b7ae71b357fdbda9ed20e20458
eli_shteinman <a0533057932@gmail.com>
2025-05-14 11:23:19 +0300

    Refactor and modularize program structure
    Reorganized classes Q1, Q2, and Q3 for better modularity and readability. Introduced a unified entry point through the `Main` method in `MyClass`. Methods are now encapsulated, promoting clearer division of responsibilities within each class.

--- Sample test/Program.cs
+++ Sample test/Program.cs
@@ -1,170 +1,175 @@
-﻿// class Q1
-// {
-//     static bool examination = false;
-//     static int number;
-//
-//     static void Main()
-//     {
-//         ReceivingInput();
-//         PrintChoice();
-//     }
-//
-//     static void ReceivingInput()
-//     {
-//         do
-//         {
-//             Console.Clear();
-//             Print("Enter a number between 1 and 50:");
-//             string choice = Console.ReadLine();
-//             examination = int.TryParse(choice, out number);
-//             if (!examination)
-//             {
-//                 Print("Number required.");
-//             }
-//             else if (number < 1 || number > 50)
-//             {
-//                 Print("Please enter a number between 1 and 50.");
-//                 examination = false;
-//             }
-//         } while (!examination);
-//     }
-//
-//     static void PrintChoice()
-//     {
-//         for (int i = 1; i <= number; i++)
-//         {
-//             if (i % 3 == 0)
-//             {
-//                 Print("Fizz");
-//                 if (i % 5 == 0)
-//                 {
-//                     Print("Buzz");
-//                 }
-//             }
-//             else
-//             {
-//                 if (i % 5 == 0)
-//                 {
-//                     Print("Buzz");
-//                 }
-//                 else
-//                 {
-//                     Print(i);
-//                 }
-//             }
-//         }
-//     }
-//     static void Print(int num)
-//     {
-//         Console.WriteLine(num);
-//     }
-//     static void Print(string str)
-//     {
-//         Console.WriteLine(str);
-//     }
-// }
+﻿class Q1
+{
+    static bool examination = false;
+    static int number;
 
-// class Q2
-// {
-//     static string input = "";
-//     static void Main()
-//     {
-//         Start();
-//     }
-//
-//     static void Start()
-//     {
-//         Console.WriteLine("Please type the sentence.");
-//         input = Console.ReadLine();
-//         string editInput = DifferentCases(input);
-//         Print(editInput);
-//     }
-//
-//     static string DifferentCases(string input)
-//     {
-//         string temp = "";
-//         int tempInt = 0;
-//         int count = 0;
-//         foreach (char c in input)
-//         {
-//             if (!char.IsLetter(c))
-//             {
-//                 tempInt = count + 1;
-//             }
-//             else if (count == tempInt)
-//             {
-//                 temp += char.ToUpper(c);
-//             }
-//             else
-//             {
-//                 temp += char.ToLower(c);
-//             }
-//             count++;
-//         }
-//         return temp;
-//     }
-//
-//     static void Print(string str)
-//     {
-//         Console.WriteLine(str);
-//     }
-// }
+    public static void Start()
+    {
+        ReceivingInput();
+        PrintChoice();
+    }
 
-// class Q3
-// {
-//     static void Main()
-//     {
-//         string input = InputUser();
-//         int count = ConsonantCount(input);
-//         Print(count);
-//     }
-//     static string InputUser()
-//     {
-//         Print("Please type what you want to check.");
-//         return Console.ReadLine();
-//     }
-//     static int ConsonantCount(string str)
-//     {
-//         int count = 0;
-//         foreach (char c in str)
-//         {
-//             switch (c)
-//             {
-//                 case 'a':
-//                     break;
-//                 case 'A':
-//                     break;
-//                 case 'e':
-//                     break;
-//                 case 'E':
-//                     break;
-//                 case 'i':
-//                     break;
-//                 case 'I':
-//                     break;
-//                 case 'o':
-//                     break;
-//                 case 'O':
-//                     break;
-//                 case 'u':
-//                     break;
-//                 case 'U':
-//                     break;
-//                 case ' ':
-//                     break;
-//                 default:
-//                     count++;
-//                     break;
-//             }
-//         }
-//         return count;
-//     }
-//     static void Print(int num)
-//     {
-//         Console.WriteLine(num);
-//     }
-//     static void Print(string str)
-//     {
-//         Console.WriteLine(str);
-//     }
-// }
+    static void ReceivingInput()
+    {
+        do
+        {
+            Console.Clear();
+            Print("Enter a number between 1 and 50:");
+            string choice = Console.ReadLine();
+            examination = int.TryParse(choice, out number);
+            if (!examination)
+            {
+                Print("Number required.");
+            }
+            else if (number < 1 || number > 50)
+            {
+                Print("Please enter a number between 1 and 50.");
+                examination = false;
+            }
+        } while (!examination);
+    }
+
+    static void PrintChoice()
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Print("Fizz");
+                if (i % 5 == 0)
+                {
+                    Print("Buzz");
+                }
+            }
+            else
+            {
+                if (i % 5 == 0)
+                {
+                    Print("Buzz");
+                }
+                else
+                {
+                    Print(i);
+                }
+            }
+        }
+    }
+    static void Print(int num)
+    {
+        Console.WriteLine(num);
+    }
+    static void Print(string str)
+    {
+        Console.WriteLine(str);
+    }
+}
+
+class Q2
+{
+    static string input = "";
+    public static void Start()
+    {
+        Console.WriteLine("Please type the sentence.");
+        input = Console.ReadLine();
+        string editInput = DifferentCases(input);
+        Print(editInput);
+    }
+
+    static string DifferentCases(string input)
+    {
+        string temp = "";
+        int tempInt = 0;
+        int count = 0;
+        foreach (char c in input)
+        {
+            if (!char.IsLetter(c))
+            {
+                tempInt = count + 1;
+            }
+            else if (count == tempInt)
+            {
+                temp += char.ToUpper(c);
+            }
+            else
+            {
+                temp += char.ToLower(c);
+            }
+            count++;
+        }
+        return temp;
+    }
+
+    static void Print(string str)
+    {
+        Console.WriteLine(str);
+    }
+}
+
+class Q3
+{
+    public static void Start()
+    {
+        string input = InputUser();
+        int count = ConsonantCount(input);
+        Print(count);
+    }
+    static string InputUser()
+    {
+        Print("Please type what you want to check.");
+        return Console.ReadLine();
+    }
+    static int ConsonantCount(string str)
+    {
+        int count = 0;
+        foreach (char c in str)
+        {
+            switch (c)
+            {
+                case 'a':
+                    break;
+                case 'A':
+                    break;
+                case 'e':
+                    break;
+                case 'E':
+                    break;
+                case 'i':
+                    break;
+                case 'I':
+                    break;
+                case 'o':
+                    break;
+                case 'O':
+                    break;
+                case 'u':
+                    break;
+                case 'U':
+                    break;
+                case ' ':
+                    break;
+                default:
+                    count++;
+                    break;
+            }
+        }
+        return count;
+    }
+    static void Print(int num)
+    {
+        Console.WriteLine(num);
+    }
+    static void Print(string str)
+    {
+        Console.WriteLine(str);
+    }
+}
+
+class MyClass
+{
+    static void Main()
+    {
+        Q1.Start();
+        Q2.Start();
+        Q3.Start();
+    }
+}
