eec4657912e080450275076a9749143eabe51f48
eli_shteinman <a0533057932@gmail.com>
2025-05-15 20:53:00 +0300

    Add initial structure for ATBASH program
    Set up the main class and skeleton methods for core functionality, including decryption, input management, content analysis, and summary handling.

--- ATBASH/Program.cs
+++ ATBASH/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿namespace ATBASH
+{
+    class myClass
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static string[] Decrypted(string[] encrypted)
+        {
+            
+        }
+        static string[] InputManager(string[] args)
+        {
+            
+        }
+        static (int, string[]) ContetAnalysis(string[] args)
+        {
+            
+        }
+        static void SummaryAndPrinting(string[] args)
+        {
+            
+        }
+    }
+}
