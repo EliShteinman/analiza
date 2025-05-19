bba5dbd9b436650e0b7d830b79e87f2c4637c123
eli_shteinman <a0533057932@gmail.com>
2025-05-19 14:38:00 +0300

    Add Program.cs to AgentOperation

---
+++ AgentOperation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace oop;
+
+class MyClass
+{
+    static void Main()
+    {
+        Agent izak = new Agent("8214453", 2);
+        Report report = new Report(izak,4,"Enemy forces detected in sector 7.");
+        MissionControl.AnalyzeReport(report);
+        IntelTools.LogTransmission(izak.codeName,IntelTools.EncryptMessage(report.summary));
+    }
+}
