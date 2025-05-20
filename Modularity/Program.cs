672503f4b357facd6669a336d2dcac1406458cf8
eli_shteinman <a0533057932@gmail.com>
2025-05-20 12:55:03 +0300

    Removing Unnecessary Update.Name and Update.Grade Calls
    The Update.Result method handles the case where there is no name and grade yet, so it can only be called

--- Modularity/Program.cs
+++ Modularity/Program.cs
@@ -5,8 +5,6 @@
         static void Main()
         {
             Student student = new Student();
-            Update.Name.Update(student);
-            Update.Grade.Update(student);
             Update.Result.Update(student);
             PrintReportStudent.Print(student);
         }
