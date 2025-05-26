6e6715bf963c6030676a5f3480367de07b25af7a
eli_shteinman <a0533057932@gmail.com>
2025-05-26 12:59:33 +0300

    Refactor Program.cs to improve clarity and maintainability
    Replaced hardcoded filename with a constant for better flexibility. Commented out console outputs to clean up the code and focus on file-based operations. These changes simplify future adjustments and improve readability.

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -4,12 +4,15 @@ class Program
 {
     public static void Main()
     {
+        const string path = "secret.txt";
         var message = MessageInput.GetMessage();
+        // Console.WriteLine(message);
         var encryptedMessage = Encrypted.Atbash(message);
-        Console.WriteLine(encryptedMessage);
-        Writer.ToFile("secret.txt", encryptedMessage);
-        var readFromFile = Reader.FromFile("secret.txt");
+        // Console.WriteLine(encryptedMessage);
+        Writer.ToFile(path, encryptedMessage);
+        var readFromFile = Reader.FromFile(path);
+        // Console.WriteLine(readFromFile);
         var decryptedMessage = Decrypted.Atbash(readFromFile);
-        Console.WriteLine(decryptedMessage);
+        // Console.WriteLine(decryptedMessage);
     }
 }
